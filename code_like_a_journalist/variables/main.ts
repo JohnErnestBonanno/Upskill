a2c5c8f60eee8fb7979e5efae3d58614f0adf18c
JohnErnestBonanno <john.ernest.bonanno@gmail.com>
2025-10-07 17:29:30 -0400

    Use constant and let type variables

--- code_like_a_journalist/variables/main.ts
+++ code_like_a_journalist/variables/main.ts
@@ -1,4 +1,19 @@
-console.log(2 + 4);
-
 // Turn on watch and check
 // deno run --watch --check main.ts
+
+const a = 2;
+const b = 40;
+let c = a + b;
+
+console.log(c);
+
+c = 10;
+console.log(c);
+
+c = c * c;
+console.log(c);
+
+// Variable Types
+
+// const: keep variables constant
+// let: overwrite variables
