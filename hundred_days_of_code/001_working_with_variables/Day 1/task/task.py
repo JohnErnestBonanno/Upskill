773ea8a6e9464427ecdd93baff76ab381d4e9b33
JohnErnestBonanno <john.ernest.bonanno@gmail.com>
2025-12-29 14:34:38 -0500

    Add Day 1 Materials to notes

---
+++ hundred_days_of_code/001_working_with_variables/Day 1/task/task.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator!")
+
+city = input("What city did you grow up in? ")
+pet_name = input("What is the name of your pet? ")
+
+band_name = city + " " + pet_name
+
+print("You play lead guitar for the " + band_name)
